4e054aeeabb2444d1fd490af63aa7640d5d37e04
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-02-29 12:37:37 +0100

    Added the stop/start scan device to plugin Android

--- src/android/AccuCheckPlugin.kt
+++ src/android/AccuCheckPlugin.kt
@@ -1,23 +1,26 @@
 package com.outsystems.experts.accucheck
 
-import android.widget.Toast
-import org.apache.cordova.CallbackContext
-import org.apache.cordova.CordovaInterface
-import org.apache.cordova.CordovaPlugin
-import org.apache.cordova.CordovaWebView
+import com.google.gson.Gson
+import com.mysugr.bluecandy.glucometer.sdk.pairing.DeviceType
+import com.mysugr.bluecandy.glucometer.sdk.pairing.GlucometerDeviceFilter
+import org.apache.cordova.*
 import org.json.JSONArray
 
 
 private const val TAG = "AccuCheckPlugin"
+private const val GET_DEVICES_SUPPORTED = "getDevicesSupported"
 private const val START_SCAN = "startScan"
+private const val STOP_SCAN = "stopScan"
 private const val CONNECT_DEVICE = "connectDevice"
 private const val READ_DATA = "readData"
+
+// Errors
+private const val ERROR_NO_DEVICES_SUPPORTED = "No devices supported!"
 
 class AccuCheckPlugin : CordovaPlugin() {
 
     override fun initialize(cordova: CordovaInterface?, webView: CordovaWebView?) {
         super.initialize(cordova, webView)
-       // init the bluecandy SDK
     }
 
     override fun execute(
@@ -25,17 +28,23 @@ class AccuCheckPlugin : CordovaPlugin() {
         args: JSONArray,
         callbackContext: CallbackContext
     ): Boolean {
-   
+
+        if (action == GET_DEVICES_SUPPORTED) {
+            this.getDevicesSupported(callbackContext)
+            return true
+        }
         if (action == START_SCAN) {
             this.startScan(callbackContext)
             return true
         }
-
+        if (action == STOP_SCAN) {
+            this.stopScan(callbackContext)
+            return true
+        }
         if (action == CONNECT_DEVICE) {
             this.connectDevice(callbackContext)
             return true
         }
-
         if (action == READ_DATA) {
             this.readData(callbackContext)
             return true
@@ -44,7 +53,20 @@ class AccuCheckPlugin : CordovaPlugin() {
         return false
     }
 
+    private fun getDevicesSupported(callbackContext: CallbackContext) {
+        val supportedDevices = GlucometerDeviceFilter.supportedGlucometerTypes
+        if (supportedDevices.isEmpty()) {
+            callbackContext.error(ERROR_NO_DEVICES_SUPPORTED)
+        } else {
+            sendSuccessResultArray(callbackContext, supportedDevices)
+        }
+    }
+
     private fun startScan(callbackContext: CallbackContext) {
+        callbackContext.success()
+    }
+
+    private fun stopScan(callbackContext: CallbackContext) {
         callbackContext.success()
     }
 
@@ -54,5 +76,11 @@ class AccuCheckPlugin : CordovaPlugin() {
 
     private fun readData(callbackContext: CallbackContext) {
         callbackContext.success()
+    }
+
+    private fun sendSuccessResultArray(callbackContext: CallbackContext, data: List<DeviceType>) {
+        val jsonResult = Gson().toJson(data)
+        val result = PluginResult(PluginResult.Status.OK, jsonResult)
+        callbackContext.sendPluginResult(result)
     }
 }
--- www/AccuCheckPlugin.js
+++ www/AccuCheckPlugin.js
@@ -4,12 +4,16 @@ exports.getDevicesSupported = function (success, error) {
     exec(success, error, 'AccuCheckPlugin', 'getDevicesSupported');
 };
 
-exports.startScan = function (success, error) {
-    exec(success, error, 'AccuCheckPlugin', 'startScan');
+exports.startScan = function (success, error, args) {
+    exec(success, error, 'AccuCheckPlugin', 'startScan',[args]);
 };
 
-exports.connectDevice = function (success, error) {
-    exec(success, error, 'AccuCheckPlugin', 'connectDevice');
+exports.stopScan = function (success, error, args) {
+    exec(success, error, 'AccuCheckPlugin', 'stopScan', [args]);
+};
+
+exports.connectDevice = function (success, error, args) {
+    exec(success, error, 'AccuCheckPlugin', 'connectDevice', [args]);
 };
 
 exports.readData = function (success, error) {
